259943550191d5dbec8c77a9cae1767627836f75
Nathan Peek <nmp14fsu@gmail.com>
2022-10-30 21:37:48 -0400

    Config webpack and single-spa

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+
+const common = require('./webpack.common');
+
+const targetPort = argv['port'] || 3000;
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  optimization: {
+    minimize: false,
+  },
+
+  cache: true,
+
+  output: {
+    publicPath: `http://localhost:${targetPort}/`,
+  },
+
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: targetPort,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Content-Type, Authorization',
+    }
+  }
+});
